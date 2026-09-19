bbe8493b6e0c83d3ff26585e210354cc6dad4316
gusdnr11111 <gusdnrgusdnr0216@gmail.com>
2026-09-19 22:59:38 +0900

    Update callback.js

--- functions/api/auth/callback.js
+++ functions/api/auth/callback.js
@@ -47,7 +47,7 @@ export async function onRequest(context) {
     return new Response(null, {
       status: 302,
       headers: {
-        'Location': '/',
+        'Location': '/?login=success',
         'Set-Cookie': cookie
       }
     });
